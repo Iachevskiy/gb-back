08a4fa61d5e8abbd21534f22010cf7e9af703cc6
vladimiriachevskii <ggg@jj.yy>
2025-02-11 14:02:21 +0500

    add gq

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,13 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 const { schema } = buildSchema(db);
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({
+    schema,
+    formatError: (err) => ({
+        message: err.message,
+        path: err.path,
+    })
+});
 const { url } = await startStandaloneServer(server);
 
 console.log(`🚀 Server ready at ${url}`);
