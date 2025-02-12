d6d3dc01ac31c995b36f5b1f51404b4ba5b28875
vladimiriachevskii <ggg@jj.yy>
2025-02-12 16:55:22 +0500

    add gq

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,23 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 const { schema } = buildSchema(db);
+
 const server = new ApolloServer({
     schema,
     formatError: (err) => ({
         message: err.message,
         path: err.path,
-    })
+    }),
 });
-const { url } = await startStandaloneServer(server);
+
+const { url } = await startStandaloneServer(server, {
+    context: async ({ req }) => {
+        // @ts-ignore
+        const { query = "", operationName = "" } = req.body;
+
+        console.log('data', typeof req, query, operationName);
+        return {};
+    },
+});
 
 console.log(`🚀 Server ready at ${url}`);
