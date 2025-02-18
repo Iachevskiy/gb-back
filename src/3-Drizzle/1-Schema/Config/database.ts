81c919e2a336aafa801db54b21ecf18fc4d72b33
vladimiriachevskii <ggg@jj.yy>
2025-02-18 14:40:34 +0500

    add deploy 222

--- src/3-Drizzle/1-Schema/Config/database.ts
+++ src/3-Drizzle/1-Schema/Config/database.ts
@@ -4,12 +4,14 @@ export const DBClient = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
 
+const ff = process.env.DATABASE_URL || "url отсутвует"
+
 DBClient.connect()
     .then(client => {
         console.log("✅ Успешное подключение к базе данных");
         client.release();
     })
     .catch(error => {
-        console.error("❌ Ошибка подключения к базе данных:", error.message);
+        console.error("❌ Ошибка подключения к базе данных:", ff, error.message);
         process.exit(1);
     });
